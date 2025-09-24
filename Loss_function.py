ea523b7e5d168d132c241d67222cbfa11c855ddb
kaviya <kaviyarajasekaran247@gmail.com>
2025-09-24 19:54:00 +0530

    Update Loss_function.py

--- Loss_function.py
+++ Loss_function.py
@@ -2,18 +2,18 @@ import torch as t
 import torch.nn as nn
 
 class FocalLoss(nn.Module):
-    def __init__(self, alfa=0.25, gamma=2):
+    def __init__(self, alfa=0.75, gamma=2):
         super().__init__()
         self.alfa = alfa
         self.gamma = gamma
-
+        
     def forward(self, y_pred, y_truth):
         y_truth = y_truth.float()
-        pt = t.where(y_truth == 1, y_pred, 1 - y_pred)
-        alfa = t.where(y_truth == 1, self.alfa, 1 - self.alfa)
-        pt = t.clamp(pt, min=1e-5)
-        loss = -(alfa * (1 - pt) ** self.gamma) * t.log(pt)
-        return loss.sum()
+        pt = torch.where(y_truth == 1, y_pred, 1 - y_pred)
+        alfa = torch.where(y_truth == 1, self.alfa, 1 - self.alfa)
+        pt = torch.clamp(pt, min=1e-5)
+        loss = -(alfa * (1 - pt) ** self.gamma) * torch.log(pt)
+        return loss.mean()
 
 if __name__ == "__main__":
     y_truth = t.tensor([[0, 1, 0],
